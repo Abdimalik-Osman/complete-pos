73ad55a12fdbb8dd0208b24deaeeda3d3802ff8a
unknown <muscab506@hotmail.com>
2022-08-28 15:08:10 +0300

    Add existing file

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,5 +1,5 @@
 const notFoundMiddleware = (req, res) => {
-  res.status(404).send("this route does not exits");
+  res.status(404).send("this route does not exitssss");
 };
 
 export default notFoundMiddleware;
--- server.js
+++ server.js
@@ -46,13 +46,12 @@ app.get("/g", (req, res) => {
 app.use(errorHandlerMiddleware);
 app.use(notFoundMiddleware);
 
-if(process.env.NODE_ENV==='production')
-{
+
     app.use('/' , express.static('client/build'))
     app.get('*' , (req,res)=>{
          res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
     }) 
-}
+
 
 
 
