3e3652889866825ed788e5ba88d46ab2f2eed15d
unknown <muscab506@hotmail.com>
2022-08-28 15:33:29 +0300

    Add existing file

--- server.js
+++ server.js
@@ -15,22 +15,17 @@ import authorization from "./middleware/auth.js";
 import bill from "./routes/bills.js";
 
 dotenv.config({ path: "./config/config.env" });
-// import helmet from "helmet";
-// import xss from "xss-clean";
-// import mongoSanitize from "express-mongo-sanitize";
+
 const app = express();
 app.use(cors());
 
 // app.use(morgan("dev"));
 app.use(express.json());
-// app.use(helmet());
-// app.use(xss());
-// app.use(mongoSanitize());
+
 
 connectDB();
-// const __dirname = dirname(fileURLToPath(import.meta.url));
-// app.use(express.static(path.join(__dirname, "./client/build")));
 const __dirname = dirname(fileURLToPath(import.meta.url))
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/products", authorization, expenseRouter);
 app.use("/api/v1/bills", authorization, bill);
@@ -46,11 +41,11 @@ app.get("/g", (req, res) => {
 app.use(errorHandlerMiddleware);
 app.use(notFoundMiddleware);
 
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-    app.use('/' , express.static('client/build'))
-    app.get('*' , (req,res)=>{
-         res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
-    }) 
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 
 
