06f546d1a2cd668a94cb28805b2b5632700bd1e7
unknown <muscab506@hotmail.com>
2022-08-28 19:22:46 +0300

    Add existing file

--- client/src/context/context.js
+++ client/src/context/context.js
@@ -51,7 +51,7 @@ export const AppProvider = ({ children }) => {
   axios.defaults.headers.common.Authorization = `Bearer ${state.token}`;
 
   const authFetch = axios.create({
-    baseURL: 'https://muscab-pos-man.herokuapp.com/api/v1/',
+    baseURL: 'https://muscab.herokuapp.com/api/v1/',
   });
 
   authFetch.interceptors.request.use(
@@ -111,7 +111,7 @@ export const AppProvider = ({ children }) => {
         data: { data },
 
         msg,
-      } = await axios.post('https://muscab-pos-man.herokuapp.com/api/v1/auth/login', loginInfo);
+      } = await axios.post('https://muscab.herokuapp.com/api/v1/auth/login', loginInfo);
 
       const res = data.data;
       const token = data.token;
