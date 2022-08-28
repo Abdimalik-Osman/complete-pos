7966a2a4fa695f285b2eca019fc08f6cf054275e
unknown <muscab506@hotmail.com>
2022-08-28 15:34:39 +0300

    Add existing file

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,5 +1,5 @@
 const notFoundMiddleware = (req, res) => {
-  res.status(404).send("this route does not exitssss");
+  res.status(404).send("this route does not exitssss--");
 };
 
 export default notFoundMiddleware;
